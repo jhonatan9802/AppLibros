6fe903c4bfec3c14c4ba4c3dbf823620ca52d15c
jhonatan9802 <astrian4576@gmail.com>
2021-12-17 20:21:51 -0500

    cambios en view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LibrosComponent } from './libros/libros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LibrosComponent},
+  {path:'libros', component:LibrosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibrosComponent } from './libros/libros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibrosComponent
   ],
   imports: [
     BrowserModule,
